2ccaf72aa55fda6d6b630fbf38f28fd1725539be
DESKTOP-HAI9B5F\tyler <Tyler.lazarus01@outlook.com>
2021-12-13 12:38:31 +0000

    TY- Commit No 5 - Frist Person Character
    Created a first person character that can walk and look around, it isn't perfect due to the requirement of making the Y value 0 to stop the player from flying makes the speed as which the person moves forward slower. Possible solution is to take the Y value and add it to the Z.

--- Assets/Scripts/Player/InputMaster.cs
+++ Assets/Scripts/Player/InputMaster.cs
@@ -1,9 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnnityEngine.Input;
+using UnityEngine.InputSystem;
 
 public class InputMaster : MonoBehaviour
 {
-    Player
+    static PlayerControlers playerControls;
+    Player player;
+    Vector2 movement;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControlers();
+        player = GetComponent<Player>();
+
+        playerControls.TourActions.Enable();
+        playerControls.UI.Disable();
+        playerControls._2DGame.Disable();
+
+        playerControls.TourActions.Movement.performed += ctx => player.InputMovement = ctx.ReadValue<Vector2>();
+        playerControls.TourActions.Movement.canceled += ctx => player.InputMovement = ctx.ReadValue<Vector2>();
+
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    CharacterController characterController;
+
+    Vector2 inputMovement;
+    public Vector2 InputMovement { set { inputMovement = value;  } }
+
+    [SerializeField] float speed = 10f;
+    [SerializeField] Camera cameraa;
+
+    private void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Movement();
+    }
+
+    void Movement()
+    {
+        float x = inputMovement.x;
+        float z = inputMovement.y;
+
+        Vector3 movement = cameraa.transform.right * x + cameraa.transform.forward * z;
+        movement.y = 0;
+
+        characterController.Move(movement * speed * Time.deltaTime);
     }
 }
